4e545e136b5cb6d829e23f8494714d0605be1268
Thiru <rthirucs@gmail.com>
2016-11-13 21:48:09 +0530

    Validating input

--- src/main/java/example/jbot/service/ServiceRequestProcessor.java
+++ src/main/java/example/jbot/service/ServiceRequestProcessor.java
@@ -4,7 +4,10 @@ import example.jbot.util.PrettyPrinter;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +16,6 @@ import java.text.MessageFormat;
 
 import static example.jbot.model.Environments.getUrlForEnvironment;
 import static java.text.MessageFormat.format;
-import static java.util.Arrays.asList;
 
 @Service
 public class ServiceRequestProcessor {
@@ -23,9 +25,12 @@ public class ServiceRequestProcessor {
         HttpEntity entity = new HttpEntity(createHeaders("admin", "secret"));
         RestTemplate restTemplate = new RestTemplate();
         String serviceRequest = getUrl(request);
-        LOGGER.info("Service request url: {}", serviceRequest);
-        ResponseEntity<String> response = restTemplate.exchange(serviceRequest, HttpMethod.GET, entity, String.class);
-        return PrettyPrinter.formatXML(response.getBody());
+        if (serviceRequest != null) {
+            LOGGER.info("Service request url: {}", serviceRequest);
+            ResponseEntity<String> response = restTemplate.exchange(serviceRequest, HttpMethod.GET, entity, String.class);
+            return PrettyPrinter.formatXML(response.getBody());
+        }
+        return "Please enter valid input. \"<env> <serviceRequestId>\" or \"internal <env> <serviceRequestId>\" or <env> livenodes";
     }
 
     private String getUrl(String request) {
@@ -35,8 +40,10 @@ public class ServiceRequestProcessor {
             return format("{0}sfm/internal/serviceRequest/{1}", url, inputs[inputs.length - 1]);
         } else if (request.contains("livenodes")) {
             return MessageFormat.format("{0}sm/admin/livenodes", url, inputs[inputs.length - 1]);
+        } else if (request.contains("serviceRequest")) {
+            return MessageFormat.format("{0}sfm/serviceRequest/{1}", url, inputs[inputs.length - 1]);
         }
-        return MessageFormat.format("{0}sfm/serviceRequest/{1}", url, inputs[inputs.length - 1]);
+        return null;
     }
 
     private HttpHeaders createHeaders(final String username, final String password) {
